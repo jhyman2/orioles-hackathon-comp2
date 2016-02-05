8209c1d53af6b723a00defa19f3367c75f1f776d
Jamison Hyman <jhyman2@umbc.edu>
2016-02-05 12:55:54 -0500

    Input for batter added

--- public/js/index.jsx
+++ public/js/index.jsx
@@ -10,9 +10,68 @@ export default class Hello extends Component {
   }
 
   render () {
+
+    const canvasStyle = {
+      border:"1px solid #000000"
+    };
+
+    const containerStyle = {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      transform: "translateY(50%)"
+    };
+
+    const canvasContainer = {
+      display: "flex",
+      justifyContent: "center"
+    };
+
+    const formStyle = {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column"
+    };
+
+    const inputContainerStyle = {
+      display: "flex",
+      flexDirection: "row",
+      padding: "10px"
+    };
+
+    const submitBtn = {
+      padding: "6px 12px",
+      background: "#1C81C9",
+      borderColor: "#1C81C9",
+      borderRadius: "4px",
+      border: "1px outset #1C81C9",
+      color: "#FFF",
+      fontSize: "20px",
+      width: "100px"
+    }
+
     return (
-      <div id="indexTest">
-        <canvas id="myCanvas" width="200" height="200" style={{border:"1px solid #000000"}}></canvas>
+      <div id="indexTest" style={containerStyle}>
+        <div id="targetBox" style={canvasContainer}>
+          <canvas id="myCanvas" width="200" height="250" style={canvasStyle}></canvas>
+        </div>
+        <form style={formStyle}>
+          <h1>Batters Information:</h1>
+          <div id="inputContainer" style={inputContainerStyle}>
+            First name:
+            <input type="text" name="firstname" />
+            Last name:
+            <input type="text" name="lastname" />
+            Balls:
+            <input type="number" name="balls" />
+            Strikes:
+            <input type="number" name="strikes" />
+            Outs:
+            <input type="number" name="outs" />
+          </div>
+          <input type="submit" value="Go" style={submitBtn} />
+        </form>
       </div>
     );
   }
